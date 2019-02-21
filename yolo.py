efe8dc159bbdcce09caac2dbcfba1da425f15fc1
koike ethan <koike.ethan@gmail.com>
2019-02-22 07:07:57 +0900

    add

--- yolo.py
+++ yolo.py
@@ -20,9 +20,9 @@ from keras.utils import multi_gpu_model
 
 class YOLO(object):
     _defaults = {
-        "model_path": 'model_data/yolo.h5',
+        "model_path": 'logs/000/trained_weights_stage_1.h5', #'model_data/yolo.h5',
         "anchors_path": 'model_data/yolo_anchors.txt',
-        "classes_path": 'model_data/coco_classes.txt',
+        "classes_path": 'model_data/voc_classes.txt', #'model_data/coco_classes.txt',
         "score" : 0.3,
         "iou" : 0.45,
         "model_image_size" : (416, 416),
