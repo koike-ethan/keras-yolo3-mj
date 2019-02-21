1169abfa8b81814ceba3998f1c274c04c0864ded
koike ethan <koike.ethan@gmail.com>
2019-02-22 06:02:26 +0900

    add

--- voc_annotation.py
+++ voc_annotation.py
@@ -3,7 +3,11 @@ from os import getcwd
 
 sets=[('2007', 'train'), ('2007', 'val'), ('2007', 'test')]
 
-classes = ["aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor"]
+classes = ["1m", "2m", "3m", "4m", "5m", "6m", "7m", "8m", "9m",
+    "1p", "2p", "3p", "4p", "5p", "6p", "7p", "8p", "9p",
+    "1s", "2s", "3s", "4s", "5s", "6s", "7s", "8s", "9s",
+    "0m", "0p", "0s",
+    "1z", "2z", "3z", "4z", "5z", "6z", "7z"]
 
 
 def convert_annotation(year, image_id, list_file):
